9b8a3e7cff5557e6e5913592f934d4b0efcd493f
yaswanth-vempuluru <yaswanthvempuluru@outlook.com>
2025-02-20 17:48:41 +0530

    added left side in auth layout ✅

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+    <h1 className="h1">
+     Store-It - The only storage solution you need
+    </h1>
+    </div>
+  );
+}
